20e6e586d157807a1617467f8e00e71fa0ad1c26
LAPTOP-712TU6OO\ASUS <ttkhk20@gmail.com>
2023-12-02 16:51:03 +0700

    chính sửa database

--- btth01_cse485.sql
+++ btth01_cse485.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2023 at 10:31 AM
+-- Generation Time: Dec 02, 2023 at 10:50 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
