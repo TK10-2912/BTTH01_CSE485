dd23fdd9d510958b551993db6ef252ae47ccafe2
LAPTOP-712TU6OO\ASUS <ttkhk20@gmail.com>
2023-12-04 15:36:14 +0700

    chỉnh sửa trang ngoài

--- admin/hearder.php
+++ admin/hearder.php
@@ -28,26 +28,37 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link " aria-current="page" href="./">Trang chủ</a>
+                        <li class="nav-item ">
+                            <a class="nav-link"
+                                style="font-weight: <?php echo basename($_SERVER['PHP_SELF']) == 'index.php' ? 'bold' : 'normal'; ?>"
+                                aria-current="page" href="./">Trang chủ</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item ">
                             <a class="nav-link" href="../index.php">Trang ngoài</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="category.php">Thể loại</a>
+                        <li class="nav-item ">
+                            <a class="nav-link"
+                                style="font-weight: <?php echo basename($_SERVER['PHP_SELF']) == 'category.php' ? 'bold' : 'normal'; ?>"
+                                href="category.php">Thể loại</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="author.php">Tác giả</a>
+                        <li class="nav-item ">
+                            <a class="nav-link"
+                                style="font-weight:<?php echo basename($_SERVER['PHP_SELF']) == 'author.php' ? 'bold' : 'normal'; ?>"
+                                href="author.php">Tác giả</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="posts.php">Bài viết</a>
+                        <li class="nav-item ">
+                            <a class="nav-link"
+                                style="font-weight:<?php echo basename($_SERVER['PHP_SELF']) == 'posts.php' ? 'bold' : 'normal'; ?>"
+                                href="posts.php">Bài viết</a>
                         </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
                             <a class="nav-link" href="logout.php">Đăng xuất</a>
                         </li>
                     </ul>
                 </div>
+
             </div>
         </nav>
     </header>
